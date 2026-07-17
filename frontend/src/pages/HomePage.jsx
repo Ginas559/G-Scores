68fd1f652cfc14356284b91fad960adb819b57e9
Ginas559 <levanphong511@gmail.com>
2026-07-17 21:30:04 +0700

    feat: implement report data fetching and display

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input, Button } from "antd";
 import { getStudentBySbd } from "../api/studentApi";
+import { getReport } from "../api/reportApi";
 const HomePage = () => {
     const [sbd, setSbd] = useState("");
     const [student, setStudent] = useState(null);
+    const [report, setReport] = useState(null);
     const findStudent = () => {
         getStudentBySbd(sbd)
             .then((response) => {
@@ -14,6 +16,19 @@ const HomePage = () => {
                 console.log(err);
             });
     };
+    const fetchReport = () => {
+        getReport()
+            .then((response) => {
+                console.log(response.data);
+                setReport(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    useEffect(() => {
+        fetchReport();
+    }, []);
     return (
         <div>
             <h1>G-Scores</h1>
@@ -41,6 +56,27 @@ const HomePage = () => {
                     <p>Geography: {student.dia_li}</p>
                     <p>Civic Education: {student.gdcd}</p>
                     <p>Foreign Language Code: {student.ma_ngoai_ngu}</p>
+                </div>
+            )}
+            {report && (
+                <div>
+                    <h2>Score Statistics</h2>
+                    {
+                        Object.keys(report).map((subject) => {
+                            console.log(report[subject]);
+                            return (
+                                <div key={subject}>
+                                    <h3>{subject}</h3>
+                                    <p>{">=8"}: {report[subject][">=8"]}</p>
+                                    <p>6-8: {report[subject]["6-8"]}</p>
+                                    <p>4-6: {report[subject]["4-6"]}</p>
+                                    <p>{"<4"}: {report[subject]["<4"]}</p>
+
+                                </div>
+                            );
+                        })
+
+                    }
                 </div>
             )}
         </div>
